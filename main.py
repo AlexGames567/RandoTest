9eb62dabb79a02e13d47209c97fda7fac3668b99
AlexGames567 <beckal@katharineum.de>
2026-09-07 15:30:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ CLASS_NAMES_PATH = "class_names.txt"
 
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model(MODEL_PATH)
+    return tf.keras.models.load_model(
+        MODEL_PATH,
+        compile=False
+        )
 
 
 def load_class_names():
